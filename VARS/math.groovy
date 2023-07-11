81c49bf8ef61cf838992a3f08bacd853a5b9d884
usharani2022 <99280045+usharani2022@users.noreply.github.com>
2023-07-11 17:22:04 +0530

    Update math.groovy

--- VARS/math.groovy
+++ VARS/math.groovy
@@ -4,13 +4,13 @@ def sum(a,b)
 }
 def sub(a,b)
 {
-  echo "sub of ${a} and ${b} is ${a-b}
+  echo "sub of ${a} and ${b} is ${a-b}"
 }
 def mul(a,b)
 {
-  echo "mul of {a} and {b} is ${a*b}
+  echo "mul of {a} and {b} is ${a*b}"
 }
 def div(a,b)
 {
-  echo "div of {a} and {b} is ${a%b}
+  echo "div of {a} and {b} is ${a%b}"
 }
